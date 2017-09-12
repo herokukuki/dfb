2a53ed2f12f2b08bba25baa5d6d9286a16d24990
mksimpler <namhuynh156@gmail.com>
2017-09-12 11:36:23 +0700

    remove json query

--- routes/human.js
+++ routes/human.js
@@ -104,28 +104,18 @@ router.get('/search', function (req, res) {
 });
 
 router.get('/:infoid', function (req, res) {
-    var asJson = req.query["json"];
-
     const type = 'human-id';
-    var infoid = req.params['infoid'];
+    const infoid = req.params['infoid'];
 
     var result = cache.get(type, infoid);
     if (result) {
-        if (asJson) {
-            res.status(200).json(result);
-        } else {
-            res.status(200).render('human/details', result);
-        }
+        res.status(200).render('human/details', result);
     } else {
         spider.crawl(type, infoid)
         .then(data => {
             if (data instanceof HumanInfo) {
                 cache.set(type, infoid, data);
-                if (asJson) {
-                    res.status(200).json(data);
-                } else {
-                    res.status(200).render('human/details', data);
-                }
+                res.status(200).render('human/details', data);
             } else {
                 res.status(404).end();
             }
